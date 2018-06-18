d52880ff768071c316ff1a4f76a1e87f5c739242
Brian Gerhards <briangerhards@gmail.com>
2018-06-18 10:07:34 -0500

    trims strings again after removing invalid characters

--- spec/parseUsdl.spec.js
+++ spec/parseUsdl.spec.js
@@ -2,32 +2,15 @@ const UsdlData1 = require('./sample/index').UsdlData1;
 const UsdlData2 = require('./sample/index').UsdlData2;
 const UsdlData_error = require('./sample/index').UsdlData_error;
 const UsdlData_invalid_characters = require('./sample/index').UsdlData_invalid_characters;
+const UsdlData_invalid_characters_2 = require('./sample/index').UsdlData_invalid_characters_2;
+const default_fixture = require('./fixtures').default;
+
 const parse = require("../index").parse;
 
 describe("USDL Parser", () => {
   it("should parse correct values", () => {
     const parsedData = parse(UsdlData1);
-    expect(parsedData.addressCity).toBe("YOUR CITY");
-    expect(parsedData.addressPostalCode).toBe("123450000");
-    expect(parsedData.addressState).toBe("NY");
-    expect(parsedData.addressStreet).toBe("2345 ANYWHERE STREET");
-    expect(parsedData.dateOfBirth).toBe('08/31/2013');
-    expect(parsedData.dateOfExpiry).toBe('08/31/2013');
-    expect(parsedData.dateOfIssue).toBe('08/31/2013');
-    expect(parsedData.documentDiscriminator).toBe("NONE");
-    expect(parsedData.documentNumber).toBe("NONE");
-    expect(parsedData.eyeColor).toBe("BRO");
-    expect(parsedData.firstName).toBe("M");
-    expect(parsedData.firstNameTruncated).toBe("N");
-    expect(parsedData.height).toBe("064 in");
-    expect(parsedData.issuer).toBe("USA");
-    expect(parsedData.jurisdictionEndorsementCodes).toBe("NONE");
-    expect(parsedData.jurisdictionRestrictionCodes).toBe("NONE");
-    expect(parsedData.lastName).toBe("Michael");
-    expect(parsedData.lastNameTruncated).toBe("N");
-    expect(parsedData.middleName).toBe("Motorist");
-    expect(parsedData.middleNameTruncated).toBe("N");
-    expect(parsedData.sex).toBe("M");
+    expect(parsedData).toEqual(default_fixture)
   });
 
   it("should correctly identify female", () => {
@@ -43,27 +26,7 @@ describe("USDL Parser", () => {
 
     const parsedData = parseData();
 
-    expect(parsedData.addressCity).toBe("YOUR CITY");
-    expect(parsedData.addressPostalCode).toBe("123450000");
-    expect(parsedData.addressState).toBe("NY");
-    expect(parsedData.addressStreet).toBe("2345 ANYWHERE STREET");
-    expect(parsedData.dateOfBirth).toBe('08/31/2013');
-    expect(parsedData.dateOfExpiry).toBe('08/31/2013');
-    expect(parsedData.dateOfIssue).toBe('08/31/2013');
-    expect(parsedData.documentDiscriminator).toBe("NONE");
-    expect(parsedData.documentNumber).toBe("NONE");
-    expect(parsedData.eyeColor).toBe("BRO");
-    expect(parsedData.firstName).toBe("M");
-    expect(parsedData.firstNameTruncated).toBe("N");
-    expect(parsedData.height).toBe("064 in");
-    expect(parsedData.issuer).toBe("USA");
-    expect(parsedData.jurisdictionEndorsementCodes).toBe("NONE");
-    expect(parsedData.jurisdictionRestrictionCodes).toBe("NONE");
-    expect(parsedData.lastName).toBe("Michael");
-    expect(parsedData.lastNameTruncated).toBe("N");
-    expect(parsedData.middleName).toBe("Motorist");
-    expect(parsedData.middleNameTruncated).toBe("N");
-    expect(parsedData.sex).toBe("M");
+    expect(parsedData).toEqual(default_fixture)
   });
 
   it("should throw error if invalid code is passed", () => {
@@ -77,26 +40,13 @@ describe("USDL Parser", () => {
   it("should remove invalid characters", () => {
     // Refer to https://www.neodynamic.com/Products/Help/BarcodeWinControl2.5/working_barcode_symbologies.htm#Pdf417
     const parsedData = parse(UsdlData_invalid_characters);
-    expect(parsedData.addressCity).toBe("YOUR CITY");
-    expect(parsedData.addressPostalCode).toBe("123450000");
-    expect(parsedData.addressState).toBe("NY");
-    expect(parsedData.addressStreet).toBe("2345 ANYWHERE STREET");
-    expect(parsedData.dateOfBirth).toBe('08/31/2013');
-    expect(parsedData.dateOfExpiry).toBe('08/31/2013');
-    expect(parsedData.dateOfIssue).toBe('08/31/2013');
-    expect(parsedData.documentDiscriminator).toBe("NONE");
-    expect(parsedData.documentNumber).toBe("NONE");
-    expect(parsedData.eyeColor).toBe("BRO");
-    expect(parsedData.firstName).toBe("M");
-    expect(parsedData.firstNameTruncated).toBe("N");
-    expect(parsedData.height).toBe("064 in");
-    expect(parsedData.issuer).toBe("USA");
-    expect(parsedData.jurisdictionEndorsementCodes).toBe("NONE");
-    expect(parsedData.jurisdictionRestrictionCodes).toBe("NONE");
-    expect(parsedData.lastName).toBe("Michael");
-    expect(parsedData.lastNameTruncated).toBe("N");
-    expect(parsedData.middleName).toBe("Motorist");
-    expect(parsedData.middleNameTruncated).toBe("N");
-    expect(parsedData.sex).toBe("M");
+
+    expect(parsedData).toEqual(default_fixture)
+  });
+
+  it("should remove invalid characters and trim when sanitizing the data", () => {
+    const parsedData = parse(UsdlData_invalid_characters_2);
+
+    expect(parsedData).toEqual(default_fixture)
   });
 });
