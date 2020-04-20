c4509c15475d50c0cdb34640a523304344e1c60f
Mark Vayngrib <mark@tradle.io>
2020-04-20 18:08:53 -0400

    set up linting, prettier

--- src/parseUsdl.js
+++ src/parseUsdl.js
@@ -1,54 +1,58 @@
-const CodeToKey = require("./keys").CodeToKey;
+const CodeToKey = require('./keys').CodeToKey
 
-const lineSeparator = "\n";
+const lineSeparator = '\n'
 
-const defaultOptions = {suppressErrors: false};
+const defaultOptions = { suppressErrors: false }
 
 exports.parse = function parseCode128(str, options = defaultOptions) {
-  const props = {};
-  const rawLines = str.trim().split(lineSeparator);
-  const lines = rawLines.map(rawLine => sanitizeData(rawLine));
-  let started;
-  lines.slice(0, -1).forEach(line => {
+  const props = {}
+  const rawLines = str.trim().split(lineSeparator)
+  const lines = rawLines.map((rawLine) => sanitizeData(rawLine))
+  let started
+  lines.slice(0, -1).forEach((line) => {
     if (!started) {
-      if (line.indexOf("ANSI ") === 0) {
-        started = true;
+      if (line.indexOf('ANSI ') === 0) {
+        started = true
       }
-      return;
+      return
     }
 
-    let code = getCode(line);
-    let value = getValue(line);
-    let key = getKey(code);
+    let code = getCode(line)
+    let value = getValue(line)
+    let key = getKey(code)
     if (!key) {
       if (options.suppressErrors) {
-        return;
+        return
       } else {
-        throw new Error("unknown code: " + code);
+        throw new Error('unknown code: ' + code)
       }
     }
 
-    if (isSexField(code)) value = getSex(code, value);
+    if (isSexField(code)) value = getSex(code, value)
 
-    props[key] = isDateField(key) ? getDateFormat(value) : value;
-  });
+    props[key] = isDateField(key) ? getDateFormat(value) : value
+  })
 
-  return props;
-};
+  return props
+}
 
-const sanitizeData = rawLine => rawLine.match(/[\011\012\015\040-\177]*/g).join('').trim();
+const sanitizeData = (rawLine) =>
+  rawLine
+    .match(/[\011\012\015\040-\177]*/g)
+    .join('')
+    .trim()
 
-const getCode = line => line.slice(0, 3);
-const getValue = line => line.slice(3);
-const getKey = code => CodeToKey[code];
+const getCode = (line) => line.slice(0, 3)
+const getValue = (line) => line.slice(3)
+const getKey = (code) => CodeToKey[code]
 
-const isSexField = code => code === "DBC";
+const isSexField = (code) => code === 'DBC'
 
-const getSex = (code, value) => (value === "1" ? "M" : "F");
+const getSex = (code, value) => (value === '1' ? 'M' : 'F')
 
-const isDateField = key => key.indexOf("date") === 0;
+const isDateField = (key) => key.indexOf('date') === 0
 
-const getDateFormat = value => {
-  const parts = [value.slice(0, 2), value.slice(2, 4), value.slice(4)];
-  return parts.join("/");
-};
+const getDateFormat = (value) => {
+  const parts = [value.slice(0, 2), value.slice(2, 4), value.slice(4)]
+  return parts.join('/')
+}
