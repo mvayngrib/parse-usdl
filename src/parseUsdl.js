63959fd653a30bb2d29b3af64685065bec34b1e9
Mark Vayngrib <mark@tradle.io>
2020-04-20 18:12:44 -0400

    refactor: use yyyy-mm-dd date format (BREAKING CHANGE)

--- src/parseUsdl.js
+++ src/parseUsdl.js
@@ -53,6 +53,6 @@ const getSex = (code, value) => (value === '1' ? 'M' : 'F')
 const isDateField = (key) => key.indexOf('date') === 0
 
 const getDateFormat = (value) => {
-  const parts = [value.slice(0, 2), value.slice(2, 4), value.slice(4)]
-  return parts.join('/')
+  const [mm, dd, yyyy] = [value.slice(0, 2), value.slice(2, 4), value.slice(4)]
+  return `${yyyy}-${mm}-${dd}`
 }
